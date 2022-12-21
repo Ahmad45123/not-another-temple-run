cb0832ee2c916a720082abd41d239c9119b8e3d2
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-12-21 12:51:31 +0200

    Added full obstacles.
    Co-authored-by: Elshimaa Betah <79271594+ShimaaBetah@users.noreply.github.com>

--- ObstacleGenerator.cpp
+++ ObstacleGenerator.cpp
@@ -1,5 +1,7 @@
 #include "ObstacleGenerator.h"
 
+#include <vector>
+
 ObstacleGenerator::ObstacleGenerator(std::vector<GroundInfo>* grounds, Gamemode mode) {
 	groundArray = grounds;
 	currentMode = mode;
@@ -17,29 +19,61 @@ void ObstacleGenerator::generateObstacles() {
 		double horizOffsets[] = { -0.06, 0, 0.06 };
 		double randomHorizontalOffset = horizOffsets[util::randomNumber(0, 2)];
 
+		bool isFullObstacle = util::randomNumber(0, 100) <= 25;
+		std::vector<std::pair<double, double>> toBeAdded;
+
 		switch (ground.dir)
 		{
 		case NEGATIVE_X:
 			x = ground.endPos + randomVerticalOffset;
-			z = ground.centerPos + randomHorizontalOffset;
+			z = ground.centerPos;
+			if (!isFullObstacle)
+				toBeAdded.push_back({ x, z + randomHorizontalOffset });
+			else {
+				toBeAdded.push_back({ x, z + horizOffsets[0] });
+				toBeAdded.push_back({ x, z + horizOffsets[1] });
+				toBeAdded.push_back({ x, z + horizOffsets[2] });
+			}
 			break;
 		case POSITIVE_X:
 			x = ground.endPos - randomVerticalOffset;
-			z = ground.centerPos + randomHorizontalOffset;
+			z = ground.centerPos;
+			if (!isFullObstacle)
+				toBeAdded.push_back({ x, z + randomHorizontalOffset });
+			else {
+				toBeAdded.push_back({ x, z + horizOffsets[0] });
+				toBeAdded.push_back({ x, z + horizOffsets[1] });
+				toBeAdded.push_back({ x, z + horizOffsets[2] });
+			}
 			break;
 		case NEGATIVE_Z:
 			z = ground.endPos + randomVerticalOffset;
-			x = ground.centerPos + randomHorizontalOffset;
+			x = ground.centerPos;
 			rot = 90;
+			if (!isFullObstacle)
+				toBeAdded.push_back({ x + randomHorizontalOffset, z });
+			else {
+				toBeAdded.push_back({ x + horizOffsets[0], z });
+				toBeAdded.push_back({ x + horizOffsets[1], z });
+				toBeAdded.push_back({ x + horizOffsets[2], z });
+			}
 			break;
 		case POSITIVE_Z:
 			z = ground.endPos - randomVerticalOffset;
-			x = ground.centerPos + randomHorizontalOffset;
+			x = ground.centerPos;
 			rot = 90;
+			if (!isFullObstacle)
+				toBeAdded.push_back({ x + randomHorizontalOffset, z });
+			else {
+				toBeAdded.push_back({ x + horizOffsets[0], z });
+				toBeAdded.push_back({ x + horizOffsets[1], z });
+				toBeAdded.push_back({ x + horizOffsets[2], z });
+			}
 			break;
 		}
 
-		obstacles.push_back(new Obstacle(Vector(x, y, z), Vector(0, rot, 0), currentMode));
+		for (auto& obs : toBeAdded)
+			obstacles.push_back(new Obstacle(Vector(obs.first, y, obs.second), Vector(0, rot, 0), currentMode));
 	}
 }
 
